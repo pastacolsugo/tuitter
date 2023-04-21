de281aee49261c10d0d742b27aa5a83f7c33b385
Ugo <ugobaroncini@gmail.com>
2023-04-21 18:21:07 +0200

    Updated seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory()->create([
-            'name' => 'test',
+            'name' => 'Signor Test',
             'username' => 'test',
             'email' => 'test@example.com',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
