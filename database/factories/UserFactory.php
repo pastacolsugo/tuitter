d3618bfa05184e9b7cfbd3269a07cbd4b511a829
Ugo <ugobaroncini@gmail.com>
2023-04-24 20:39:52 +0200

    Added field to db for profile picture asset path

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'profile_pic_asset' => array_rand($this->profile_pictures_assets, 1)
         ];
     }
 
@@ -36,4 +37,14 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
+    private $profile_pictures_assets = array(
+        'profile_pictures/cat_0.jpeg',
+        'profile_pictures/cat_1.jpeg',
+        'profile_pictures/cat_2.jpeg',
+        'profile_pictures/cat_3.jpeg',
+        'profile_pictures/cat_4.jpeg',
+        'profile_pictures/cat_5.jpeg',
+        'profile_pictures/cat_0.jpeg',
+    );
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,9 @@ return new class extends Migration
             $table->string('name');
             $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile_pic_asset');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
